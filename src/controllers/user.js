79ee7e6ad206a5e783231cae0b25a0d7ba9fb00d
josemnr <maninari97@gmail.com>
2021-05-11 14:00:11 -0500

    Update user controller

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -4,17 +4,17 @@ const bcrypt = require('bcryptjs');
 
 class UserController {
     async createUser({ user }) {
-        const { name, email, password, role } = user;
+        const { password } = user;
         const hashedPassword = await bcrypt.hash(password, 10);
-        const createdUserId = await userModel.create({
-            email,
-            name,
-            password: hashedPassword,
-            role: role,
-            google_id: "",
-            profile_pic: "/public/images/profilePics/generic.jpg",
-            greenhouses_id: []
-        });
+        user.password = hashedPassword;
+        user.profile_pic = "https://ifarm-app-images.s3.amazonaws.com/images/profilePics/generic.jpg"
+        if(!user.google_id) {
+            user.google_id = "";
+        }
+        if(!user.greenhouses_id){
+            user.greenhouses_id = [];
+        }
+        const createdUserId = await userModel.create(user);
         return createdUserId;
     }
 
